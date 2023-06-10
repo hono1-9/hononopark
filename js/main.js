e604c1a1b194782dee07562536d3c4bd30e51240
hono1-9 <hono07312003@gmail.com>
2023-06-10 23:51:25 +0900

    公園一覧,js追加,各写真文章確定

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function($){
+
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function(){
+        var adjust=0;
+        var speed=1000;
+        var href=$(this).attr("href");
+        var target=$(href=="#" || href=="" ? 'html':href);
+        var position=target.offset().top-adjust;
+        $('body,html').animate({
+            scrollTop:position
+        },speed,'swing');
+        return false;
+    })
+
+    
+});
